9debf5e9196ec49a4d2b34c238d2769292c55954
amora7741 <mora7741@gmail.com>
2024-06-25 16:55:34 -0700

    Implement zod validation

--- src/components/ToDoDialog.tsx
+++ src/components/ToDoDialog.tsx
@@ -11,7 +11,42 @@ import { Input } from './ui/input';
 import { Label } from './ui/label';
 import { Textarea } from './ui/textarea';
 
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+
+const formSchema = z.object({
+  title: z
+    .string()
+    .min(1, { message: 'You must have a title.' })
+    .max(50, { message: 'Your title must be less than 50 characters.' }),
+  description: z
+    .string()
+    .max(50, { message: 'Your title must be less than 50 characters.' }),
+});
+
 const ToDoDialog = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: '',
+      description: '',
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -24,24 +59,51 @@ const ToDoDialog = () => {
           <DialogTitle className='text-3xl font-bold'>
             Create your task
           </DialogTitle>
-          <div className='grid w-full items-center gap-2 pt-8'>
-            <Label htmlFor='taskname'>Task Title</Label>
-            <Input
-              type='text'
-              id='taskname'
-              placeholder='Task'
-              className='bg-black/20 border-none w-full'
-            />
-          </div>
-          <div className='grid w-full items-center gap-2 pt-4'>
-            <Label htmlFor='taskname'>Task Description</Label>
-            <Textarea
-              id='taskname'
-              placeholder='Description'
-              className='bg-black/20 border-none w-full resize-none'
-            />
-          </div>
         </DialogHeader>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className='flex flex-col space-y-8'
+          >
+            <FormField
+              control={form.control}
+              name='title'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Task Title</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder='Task'
+                      className='bg-black/20 border-none w-full'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className='absolute right-6 text-purple-300' />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='description'
+              render={({ field }) => (
+                <FormItem className='pb-2'>
+                  <FormLabel>Task Description</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder='Description'
+                      className='bg-black/20 border-none w-full resize-none'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className='absolute right-6 text-purple-300' />
+                </FormItem>
+              )}
+            />
+            <Button className='bg-black/50 hover:bg-black/30 self-end'>
+              Submit
+            </Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
