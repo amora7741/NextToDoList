ca623f1f4a828c6c8eaafe74f4136bdb85977917
amora7741 <mora7741@gmail.com>
2024-06-25 17:02:51 -0700

    Make seperate form component

--- src/components/ToDoDialog.tsx
+++ src/components/ToDoDialog.tsx
@@ -7,46 +7,10 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
-import { Input } from './ui/input';
-import { Label } from './ui/label';
-import { Textarea } from './ui/textarea';
 
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
-
-const formSchema = z.object({
-  title: z
-    .string()
-    .min(1, { message: 'You must have a title.' })
-    .max(50, { message: 'Your title must be less than 50 characters.' }),
-  description: z
-    .string()
-    .max(50, { message: 'Your title must be less than 50 characters.' }),
-});
+import ToDoForm from './ToDoForm';
 
 const ToDoDialog = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      title: '',
-      description: '',
-    },
-  });
-
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
-  };
-
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -60,50 +24,7 @@ const ToDoDialog = () => {
             Create your task
           </DialogTitle>
         </DialogHeader>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className='flex flex-col space-y-8'
-          >
-            <FormField
-              control={form.control}
-              name='title'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Task Title</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder='Task'
-                      className='bg-black/20 border-none w-full'
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage className='absolute right-6 text-purple-300' />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name='description'
-              render={({ field }) => (
-                <FormItem className='pb-2'>
-                  <FormLabel>Task Description</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      placeholder='Description'
-                      className='bg-black/20 border-none w-full resize-none'
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage className='absolute right-6 text-purple-300' />
-                </FormItem>
-              )}
-            />
-            <Button className='bg-black/50 hover:bg-black/30 self-end'>
-              Submit
-            </Button>
-          </form>
-        </Form>
+        <ToDoForm />
       </DialogContent>
     </Dialog>
   );
---
+++ src/components/ToDoForm.tsx
@@ -0,0 +1,87 @@
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from './ui/input';
+import { Textarea } from './ui/textarea';
+import { Button } from './ui/button';
+
+const formSchema = z.object({
+  title: z
+    .string()
+    .min(1, { message: 'You must have a title.' })
+    .max(50, { message: 'Your title must be less than 50 characters.' }),
+  description: z
+    .string()
+    .max(50, { message: 'Your title must be less than 50 characters.' }),
+});
+
+const ToDoForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: '',
+      description: '',
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='flex flex-col space-y-8'
+      >
+        <FormField
+          control={form.control}
+          name='title'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Task Title</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder='Task'
+                  className='bg-black/20 border-none w-full'
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className='absolute right-6 text-purple-300' />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='description'
+          render={({ field }) => (
+            <FormItem className='pb-2'>
+              <FormLabel>Task Description</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder='Description'
+                  className='bg-black/20 border-none w-full resize-none'
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className='absolute right-6 text-purple-300' />
+            </FormItem>
+          )}
+        />
+        <Button className='bg-black/50 hover:bg-black/30 self-center w-1/2'>
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default ToDoForm;
