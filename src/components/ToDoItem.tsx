f3a16a58e6e6a0adbda13d9d0fb673648dae0a2c
amora7741 <mora7741@gmail.com>
2024-07-01 13:47:32 -0700

    Add setting a task to complete

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -6,18 +6,39 @@ import { FaCheck } from 'react-icons/fa';
 const ToDoItem = ({
   task,
   deleteTask,
+  completeTask,
 }: {
   task: Todo;
   deleteTask: () => void;
+  completeTask: () => void;
 }) => {
   return (
-    <li className='bg-black/40 p-4 rounded-md flex flex-col space-y-4 justify-between break-words'>
+    <li
+      className={`${
+        task.completed ? 'bg-black/10' : 'bg-black/40'
+      }  p-4 rounded-md flex flex-col space-y-4 justify-between break-words`}
+    >
       <div>
-        <p className='text-white font-semibold text-3xl'>{task.title}</p>
-        <p className='text-white text-xl italic'>{task.description}</p>
+        <p
+          className={`${
+            task.completed ? 'line-through text-white/50' : ''
+          }  text-white font-semibold text-3xl`}
+        >
+          {task.title}
+        </p>
+        <p
+          className={`${
+            task.completed ? 'line-through text-white/50' : ''
+          } text-xl italic text-white`}
+        >
+          {task.description}
+        </p>
       </div>
       <div className='flex space-x-2 ml-auto'>
-        <Button className='bg-black/50 hover:bg-black/30'>
+        <Button
+          className='bg-black/50 hover:bg-black/30'
+          onClick={completeTask}
+        >
           <FaCheck className='fill-green-500 w-5 h-auto' />
         </Button>
         <Button className='bg-black/50 hover:bg-black/30' onClick={deleteTask}>
--- src/components/ToDoWrapper.tsx
+++ src/components/ToDoWrapper.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ToDoDialog from './ToDoDialog';
 import ToDoItem from './ToDoItem';
 
@@ -23,6 +23,14 @@ const ToDoWrapper = () => {
     setTasks(newTasks);
   };
 
+  const completeTask = (taskId: string) => {
+    const newTasks = tasks.map((t) =>
+      t.id === taskId ? { ...t, completed: !t.completed } : t
+    );
+
+    setTasks(newTasks);
+  };
+
   return (
     <div className='flex flex-col gap-16 z-50 w-full max-w-3xl h-fit bg-black/30 px-4 py-8 sm:p-10 backdrop-blur-sm rounded-lg'>
       <div className='relative flex flex-col gap-4 sm:flex-row items-center text-center'>
@@ -32,12 +40,13 @@ const ToDoWrapper = () => {
         <ToDoDialog addTask={addTask} />
       </div>
       {tasks.length > 0 && (
-        <ul className='space-y-4'>
+        <ul className='space-y-4 max-h-[70vh] overflow-auto'>
           {tasks.map((task) => (
             <ToDoItem
               task={task}
               key={task.id}
               deleteTask={() => deleteTask(task.id)}
+              completeTask={() => completeTask(task.id)}
             />
           ))}
         </ul>
